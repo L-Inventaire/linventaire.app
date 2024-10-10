34c058fd717d31d1e0d61dfc3708cc631c2e7c82
Ilya Ukhanov <ukhanov.ilya.67@outlook.com>
2024-10-10 13:07:46 +0200

    Small cleanup

--- src/views/client/modules/document/signed.tsx
+++ src/views/client/modules/document/signed.tsx
@@ -1,19 +1,19 @@
 import { Button } from "@atoms/button/button";
+import { Loader } from "@atoms/loader";
 import { PageLoader } from "@atoms/page-loader";
-import { Base, Section, Title } from "@atoms/text";
+import { Base, Section } from "@atoms/text";
 import { SigningSessionsApiClient } from "@features/documents/api-client/api-client";
 import { useSigningSession } from "@features/documents/hooks";
+import { Invoices } from "@features/invoices/types/types";
 import { isErrorResponse } from "@features/utils/rest/types/types";
+import { AspectRatio } from "@radix-ui/themes";
 import { Page } from "@views/client/_layout/page";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
-import styles from "./index.module.css";
 import { TitleBar } from "./components/title-bar";
-import { Invoices } from "@features/invoices/types/types";
-import { Loader } from "@atoms/loader";
-import { AspectRatio } from "@radix-ui/themes";
+import styles from "./index.module.css";
 
 export const SignedSessionPage = () => {
   const { session: sessionID } = useParams();
@@ -86,12 +86,6 @@ export const SignedSessionPage = () => {
       >
         <div className="w-full h-full flex flex-col items-center">
           <div className="w-3/4 bg-white flex flex-col justify-between items-center rounded-md p-6">
-            {/* <div className="flex flex-col justify-center items-center mb-6">
-              <Title>Document signé</Title>
-              <Section>
-                Veuillez retrouver ci-dessous le document duement signé
-              </Section>
-            </div> */}
             <TitleBar
               className={"mb-8"}
               signingSession={signingSession}
