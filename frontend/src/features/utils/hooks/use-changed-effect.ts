67a1b125e5b1c1ecc23414e6406d967ea62fcaea
Romaric Mourgues <romaric.mollard@gmail.com>
2025-10-23 09:52:00 +0200

    fix: make sure a change happened before changing address

--- frontend/src/features/utils/hooks/use-changed-effect.ts
+++ frontend/src/features/utils/hooks/use-changed-effect.ts
@@ -2,16 +2,25 @@ import _ from "lodash";
 import { useEffect, useRef } from "react";
 
 /**
- * Trigger only when value changed
+ * Trigger only when value changed and not when it initialized (undefined / null to value)
  */
 export const useEffectChange = <T>(
   callback: (arg: T[]) => void | (() => void),
   value: T[] = []
 ) => {
-  const previousValue = useRef(value);
+  const previousValue = useRef<T[]>(value);
   useEffect(() => {
     const tmp = previousValue.current;
-    if (_.isEqual(value, tmp)) return;
+    if (
+      previousValue.current.every((val) => {
+        return (
+          val === undefined ||
+          val === null ||
+          _.isEqual(val, value[tmp.indexOf(val)])
+        );
+      })
+    )
+      return;
     previousValue.current = _.cloneDeep(value);
     return callback(tmp);
   }, value);
